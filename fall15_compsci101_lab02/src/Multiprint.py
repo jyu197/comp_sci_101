aef29a8051cafa42a8574cbea38c2a60f69c7dcb
jyu197 <jyu197@gmail.com>
2015-09-04 17:46:46 -0400

    Update Multiprint in lab02

--- fall15_compsci101_lab02/src/Multiprint.py
+++ fall15_compsci101_lab02/src/Multiprint.py
@@ -12,28 +12,36 @@ def printit():
     global count,word
     print count,"\t",word
     count = count + 1
+
 def f1():
-    f2()
-    f2()
     f2()
     f2()
 
 def f2():
     f3()
     f3()
-    f3()
-    f3()
 
 def f3():
-    printit()
-    printit()
-    printit()
-    printit()
+    f4()
+    f4()
     
+def f4():
+    f5()
+    f5()
+    
+def f5():
+    f6()
+    f6()
+    
+def f6():
+    f7()
+    f7()
+    
+def f7():
+    printit()
+    printit()
     
 def verse():
-    f1()
-    f1()
     f1()
     f1()
     
